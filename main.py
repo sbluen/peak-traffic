c4c64c59204ba9f5e7846574a4e78caa570037db
unknown <sbluen@.musicreports.com>
2015-05-15 16:09:23 -0700

    add initial processing step

--- main.py
+++ main.py
@@ -17,10 +17,15 @@ class Node():
 import sys
 with open(sys.argv[1], "r") as f:
     for line in f:
+        
         sender = line.split()[6].split("@")[0]
+        recip = line.split()[7].split("@")[0]
+        
+        if sender == recip:
+            continue
+        
         if sender not in nodes:
             nodes[sender] = Node(sender)
-        recip = line.split()[7].split("@")[0]
         if recip not in nodes:
             nodes[recip] = Node(recip)
         
@@ -41,4 +46,26 @@ with open(sys.argv[1], "r") as f:
             
         nodes[sender].outg[recip] = (nodes[recip])
 
-#TODO: Analysis code
+
+groups = [None] * (len(nodes)+1)
+
+#put groups of 3 nodes in variable groups
+
+if len(nodes) < 3:
+    #degenerate case
+    pass
+else:
+    for node in nodes.values():
+        for link1 in node.edges.values():
+            for link2 in link1.edges.values():
+                if link2 == node:
+                    #This is the link back to node
+                    continue
+                if node in link2.edges.values():
+                    #sorted by name
+                    groups[3].append(sorted([node, link1, link2]))
+
+havework = True
+while havework:
+    havework = False
+    
